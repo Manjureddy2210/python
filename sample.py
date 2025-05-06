aea15247f2e198d479c2c0b4b39d3f1f81eb652a
Manjureddy2210 <manjulanancharla10@gmail.com>
2025-05-06 21:04:23 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,11 @@
+# expected input:5
+#output:"you entered:5"
+num=int(input("give  value of num:"))
+print("you entered:",num)
+
+#input:3.14
+#output:"value of pi:3.14"
+pi=float(input("pi:"))
+print("value of pi:",pi)
+
+
